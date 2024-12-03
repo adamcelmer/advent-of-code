3c843242a69636b9ee892c470520b850750553db
Adam Celmer <adam.celmer@unblu.com>
2024-12-03 17:54:09 +0100

    2024 - day 2 part 2

--- 2024/src/day-2/solution.py
+++ 2024/src/day-2/solution.py
@@ -25,48 +25,13 @@ def is_safe(report) -> bool:
     return True
 
 
-def is_safe_nested(report, already_failed_once=False) -> bool:
-    is_increasing = None
-    for i in range(0, len(report) - 1):
-        diff = report[i + 1] - report[i]
-        if is_increasing is None:
-            is_increasing = diff > 0
-        is_increased = diff > 0
-        if is_increased != is_increasing or not 1 <= abs(diff) <= 3:
-            if not already_failed_once:
-                without_i = is_safe_nested(report[:i] + report[i + 1:], True)
-                if without_i:
-                    # print(f'{report} works without the {i} element')
-                    return True
-                without_i_plus_1 = is_safe_nested(report[:i + 1] + report[i + 2:], True)
-                if without_i_plus_1:
-                    # print(f'{report} works without the {i + 1} element')
-                    return True
-                print(f'Invalid report: {report}')
-            return False
-    return True
-
-
-# def is_safe_with_retry(report: list[int]) -> bool:
-#     already_failed_once = False
-#     is_increasing = None
-#     i = 0
-#     while i < len(report) - 1:
-#         diff = report[i + 1] - report[i]
-#         if is_increasing is None:
-#             is_increasing = diff > 0
-#         is_increased = diff > 0
-#         if is_increased != is_increasing or not 1 <= abs(diff) <= 3:
-#             if not already_failed_once:
-#                 print(f'Found invalid report {report}. Dropping element {i}: {report[i]}')
-#                 already_failed_once = True
-#                 report.pop(i)
-#                 print(report)
-#                 i = 0
-#                 continue
-#             return False
-#         i = i + 1
-#     return True
+def is_safe_nested(report) -> bool:
+    if is_safe(report):
+        return True
+    for i in range(0, len(report)):
+        if is_safe(report[:i] + report[i + 1:]):
+            return True
+    return False
 
 
 def part_1(reports: list[list[int]]):
@@ -77,7 +42,6 @@ def part_1(reports: list[list[int]]):
 def part_2(reports: list[list[int]]):
     safe_reports = list(filter(lambda report: is_safe_nested(report), reports))
     print(len(safe_reports))
-    print(safe_reports)
 
 
 if __name__ == '__main__':
