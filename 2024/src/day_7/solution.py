f6995568d2ef578e0797e377abf00b88b5ffc07e
Adam Celmer <adamcelmer94@gmail.com>
2024-12-07 16:35:12 +0100

    2024 - day 7 - clean up

--- 2024/src/day_7/solution.py
+++ 2024/src/day_7/solution.py
@@ -5,29 +5,20 @@ class Equation:
     expected_result: int
     elements: list[int]
 
-    def __can_get_result(self, total, elements):
+    def __can_get_result(self, total, elements, with_concatenation=False):
         if total == self.expected_result:
             return True
         if not elements:
             return False
-        return self.__can_get_result(total + elements[0], elements[1:]) or self.__can_get_result(total * elements[0], elements[1:])
+        concatenated_result = self.__can_get_result(int(f'{total}{elements[0]}'), elements[1:], with_concatenation=with_concatenation) if with_concatenation else False
+        return (self.__can_get_result(total + elements[0], elements[1:], with_concatenation=with_concatenation)
+                or self.__can_get_result(total * elements[0], elements[1:], with_concatenation=with_concatenation)
+                or concatenated_result)
 
 
-    def __can_get_result_with_concatenation(self, total, elements):
-        if total == self.expected_result:
-            return True
-        if not elements or total > self.expected_result:
-            return False
-        return (self.__can_get_result_with_concatenation(total + elements[0], elements[1:])
-                or self.__can_get_result_with_concatenation(total * elements[0], elements[1:])
-                or self.__can_get_result_with_concatenation(int(f'{total}{elements[0]}'), elements[1:]))
-
-
-    def is_valid(self) -> bool:
-        return self.__can_get_result(self.elements[0], self.elements[1:])
-
-    def is_valid_with_concatenation(self) -> bool:
-        return self.__can_get_result_with_concatenation(self.elements[0], self.elements[1:]) or self.__can_get_result_with_concatenation(int(f'{self.elements[0]}{self.elements[1]}'), self.elements[2:])
+    def is_valid(self, with_concatenation=False) -> bool:
+        concatenated_result = self.__can_get_result(int(f'{self.elements[0]}{self.elements[1]}'), self.elements[2:], with_concatenation=with_concatenation) if with_concatenation else False
+        return self.__can_get_result(self.elements[0], self.elements[1:], with_concatenation=with_concatenation) or concatenated_result
 
     def print(self):
         print(f'expected_result: {self.expected_result}, elements: {self.elements}')
@@ -55,7 +46,7 @@ def part_1(equations: list[Equation]):
 def part_2(equations: list[Equation]):
     total = 0
     for e in equations:
-        if e.is_valid_with_concatenation():
+        if e.is_valid(with_concatenation=True):
             total = total + e.expected_result
     print(f'Part 2: {total}')
 
