20c09136f4976ecc336454752c26b2afc4eb05e8
Adam Celmer <adamcelmer94@gmail.com>
2024-12-11 21:45:26 +0100

    2024 - day 8

--- 2024/src/day_11/solution.py
+++ 2024/src/day_11/solution.py
@@ -1,5 +1,10 @@
+from time import time
+
+def current_milli_time():
+    return round(time() * 1000)
+
 def parse_input():
-    with open('input-demo.txt', 'r') as f:
+    with open('input.txt', 'r') as f:
         return f.read().split()
 
 
@@ -28,4 +33,30 @@ def part_1():
         stones = new_stones
     print(f'Part 1: {len(stones)}')
 
+
+def part_2():
+    stones = {k: 1 for k in parse_input()}
+
+    for _ in range(75):
+        new_stones = {}
+        for stone, count in stones.items():
+            child_stones = process_stone(stone)
+            for child_stone in child_stones:
+                if child_stone in new_stones:
+                    new_stones[child_stone] += count
+                else:
+                    new_stones[child_stone] = count
+        stones = new_stones
+
+    print(f'Part 2: {sum(stones.values())}')
+
+
+start = current_milli_time()
 part_1()
+end = current_milli_time()
+print(f'Took: {end - start}ms')
+
+start = current_milli_time()
+part_2()
+end = current_milli_time()
+print(f'Took: {end - start}ms')
